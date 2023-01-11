5e6ce3c809850feb85f2cfb8e8b2ba1bc69bbc45
tjcoppola234 <37402171+tjcoppola234@users.noreply.github.com>
2023-01-11 13:45:24 -0500

    Set up calendar

--- main.rb
+++ main.rb
@@ -5,6 +5,16 @@ require 'icalendar'
 courseNames = File.open("course_sections.txt").readlines.map(&:chomp)
 p courseNames
 
+courseCal = Icalendar::Calendar.new 
+
 courseInfoRaw = Net::HTTP.get(URI("https://courselistings.wpi.edu/assets/prod-data.json"))
 courseInfoParsed = JSON.parse(courseInfoRaw)
-p courseInfoParsed["Report_Entry"].select { |c| c["Course_Section"].start_with?(courseNames[0]) }
+courseNames.each { |name|
+    currCourse = courseInfoParsed["Report_Entry"].select { |c| c["Course_Section"].start_with?(name) }
+    courseCal.event do |e|
+        e.description = "This is course #{name}"
+        e.dtstart = DateTime.civil(2023, 1, 13, 6, 30)
+    end
+}
+
+courseCal.publish
