4f1ba386e236449c758d893d73dfffd24fa71177
tjcoppola234 <37402171+tjcoppola234@users.noreply.github.com>
2023-03-05 15:34:43 -0500

    days of week accounted for

--- WPICourse2Calendar.rb
+++ WPICourse2Calendar.rb
@@ -4,6 +4,9 @@ require 'icalendar-rrule'
 
 using Icalendar::Scannable
 
+# Maps day letters to icalendar rrule format
+rruleDays = {"M" => "MO", "T" => "TU", "W" => "WE", "R" => "TH", "F" => "FR"}
+
 courseNames = File.open("course_sections.txt").readlines.map(&:chomp)
 p courseNames
 
@@ -17,19 +20,27 @@ courseNames.each { |name|
         # The location, days of the week, and class times of the course
         detSplit = det.split(" | ")
         detLoc = detSplit[0]
-        detDays = detSplit[1]
-        # Get the start time and end time (Ex: [["9","00"], ["9","50"]])
+        detDays = []
+        detSplit[1].split("-").each_with_index do |d, i|
+            detDays[i] = rruleDays[d]
+        end
+        p detDays
+        detDays = detDays.join(",")
+        p detDays
+
+        # Get the start time and end time for each class day (Ex: [["9","00"], ["9","50"]])
         detTimes = detSplit[2].split(" ")
         detTimes = [detTimes[0].split(":"), detTimes[-2].split(":")]
         # The first day of the course
         startDate = currCourse["Course_Section_Start_Date"].split("-")
+        endDate = currCourse["Course_Section_Start_Date"].split("-")
 
         courseCal.event do |e|
-            e.description = currCourse["Course_Title"]
+            e.summary = currCourse["Course_Title"]
             e.dtstart = DateTime.civil(startDate[0].to_i, startDate[1].to_i, startDate[2].to_i, detTimes[0][0].to_i, detTimes[0][1].to_i)
             e.dtend = DateTime.civil(startDate[0].to_i, startDate[1].to_i, startDate[2].to_i, detTimes[1][0].to_i, detTimes[1][1].to_i)
             e.location = detLoc;
-            e.rrule = "FREQ=WEEKLY;BYDAY=MO,WE;" # need to add UNTIL field
+            e.rrule = "FREQ=WEEKLY;BYDAY=#{detDays};" # need to add UNTIL field
         end 
     }
 }
