23221fef32ad55621ee9f8950266cb627c97b65c
tjcoppola234 <tjcoppola234@gmail.com>
2023-04-11 19:25:55 -0400

    Roadblock: unable to fetch json

--- js/src/App.js
+++ js/src/App.js
@@ -1,5 +1,6 @@
 import './App.css';
 import { useState } from 'react';
+import { createEvent } from 'ics';
 
 function App() {
   const [courses, setCourses] = useState(["course1"])
@@ -8,7 +9,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <p>Enter your course id</p>
-        <div>
+        <div id='courseList'>
           {courses.map(i => <input id={i} type="text"/>)}
         </div>
         <div style={{display:"flex", flexDirection: "row"}}>
@@ -21,9 +22,57 @@ function App() {
   );
 }
 
-function makeical(courses) {
+async function makeical(courses) {
+  // Maps day letters to icalendar rrule format
+  const rruleDays = {"M" : "MO", "T" : "TU", "W" : "WE", "R" : "TH", "F" : "FR"}
+  
+  // Read the courses from input
+  const docCourseList = document.getElementById("courseList").children;
+  let courseList = []
+  for(let i = 0; i < docCourseList.length; i++) {
+    const courseName = docCourseList[i].value.split(" ")
+    courseList[i] = [courseName[0] + " " + courseName[1], courseName[2]];
+  }
+  
+  // Load raw course data
+  fetch("https://courselistings.wpi.edu/assets/prod-data-raw.json", {
+    headers: {'mode': 'no-cors'}
+  })
+  .then(res => res.json())
+  .then(json => {
+    for(let course of courseList) {
+      let currCourse = json.Report_Entry.find(c => c.Course_Section.startsWith(course[0]) && c.Offering_Period.startsWith(course[1]))
+      console.log(currCourse)
+    }
+  })
 
+  const event = {
+    start: [2018, 10, 15],
+    end: [2018, 10, 16],
+    recurrenceRule: "FREQ=WEEKLY;UNTIL=20230503T000000;BYDAY=MO,TH"
+  }
+  
+  // Implementation from https://github.com/adamgibbons/ics#using-esmodules--in-the-browser
+  // const filename = 'Course_Schedule.ics'
+  // const file = await new Promise((resolve, reject) => {
+  //   createEvent(event, (error, value) => {
+  //     if (error) {
+  //       reject(error)
+  //     }
+  //     resolve(new File([value], filename, { type: 'plain/text' }))
+  //   })
+  // })
+  // const url = URL.createObjectURL(file);
+  // const anchor = document.createElement('a');
+  // anchor.href = url;
+  // anchor.download = filename;
+  // document.body.appendChild(anchor);
+  // anchor.click();
+  // document.body.removeChild(anchor);
+  
+  // URL.revokeObjectURL(url);
 }
+
 function addCourseInput(courses) {
   let newInput = `course${courses.length + 1}`;
   courses.push(newInput);
